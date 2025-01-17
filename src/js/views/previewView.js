39c38be37c17da0f44bd5f281e60e1b28efd639c
akshayzagad <akshayzagade62@gmail.com>
2025-01-17 22:28:42 +0530

    create bookmark view to render bookmarks

---
+++ src/js/views/previewView.js
@@ -0,0 +1,37 @@
+/**
+ * Class representing the view for displaying search results.
+ * @extends View
+ */
+import View from "./View";
+
+class PreviewView extends View {
+    _parentElement = ''; 
+
+    // _errorMessage = "No recipes yet bookmarked. Please select recipe to bookmarked !";
+    
+  /**
+   * Generates the markup for the results view.
+   * 
+   * This method maps over the data and generates a markup preview for each item,
+   * then joins them into a single string.
+   * 
+   * @retxurns {string} The generated markup as a single string.
+   */
+   _genrateMarkup() {
+      const id = window.location.hash.slice(1);
+      return `<li class="preview">
+      <a class="preview__link ${this.data.id === id ?'preview__link--active' : ''} " href="#${this.data.id}">
+        <figure class="preview__fig">
+          <img src="${this.data.imageurl}" alt="Test" />
+        </figure>
+        <div class="preview__data">
+          <h4 class="preview__title">${this.data.title}</h4>
+          <p class="preview__publisher">${this.data.publisher}</p>
+        </div>
+      </a>
+    </li>`;
+   }
+   
+}
+
+export default new PreviewView();
