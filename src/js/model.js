cccb0b65430b3d5f2a6611fb706ed99604706d29
akshayzagad <akshayzagade62@gmail.com>
2025-01-27 22:04:45 +0530

    Implement Correct function naming in addrecipevivew class (_addHandlerShowWindow , _addHandlerHideWindow)

--- src/js/model.js
+++ src/js/model.js
@@ -1,36 +1,5 @@
-/**
- * State object to store recipe and search results.
- * @typedef {Object} State
- * @property {Object} recipe - The current recipe details.
- * @property {Object} searchs - The search results and query details.
- * @property {string} searchs.query - The search query string.
- * @property {Array<Object>} searchs.results - The search results array.
- * @property {number} searchs.page - The current search results page.
- * @property {number} searchs.resultsPerPage - The number of results per page.
- */
-
-/**
- * Loads a recipe by its ID and updates the state.
- * @async
- * @param {string} id - The ID of the recipe to load.
- * @throws Will throw an error if the recipe cannot be loaded.
- */
-
-/**
- * Loads search results based on a query and updates the state.
- * @async
- * @param {string} query - The search query string.
- * @throws Will throw an error if the search results cannot be loaded.
- */
-
-/**
- * Gets search results for a specific page.
- * @param {number} [page=state.searchs.page] - The page number to get results for.
- * @returns {Array<Object>} The search results for the specified page.
- */
 import { API_URL, RES_PER_PAGE, KEY } from "./config";
-// import { getJson,sendJSON } from "./helpers";
-import{AJAX} from "./helpers";
+import { AJAX } from "./helpers";
 
 /**
  * The state object holds the application's state data.
@@ -43,200 +12,200 @@ import{AJAX} from "./helpers";
  * @property {number} searchs.resultsPerPage - The number of results per page.
  */
 export const state = {
-    recipe: {},
-    searchs: {
-        query: '',
-        results: [],
-        page: 1,
-        resultsPerPage: RES_PER_PAGE,
-    },
-    bookmarks: [],
-    // bookmarked : false,
+  recipe: {},
+  searchs: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
+  bookmarks: [],
 };
 
+/**
+ * Creates a recipe object from the provided data.
+ * 
+ * @param {Object} data - The data to create the recipe object from.
+ * @returns {Object} The created recipe object.
+ */
 const createRecipeObject = function (data) {
-    /**
-            * Destructures the recipe object from the response data.
-            * 
-            * @param {Object} responseData - The response data from the API.
-            * @param {Object} responseData.data - The data object containing the recipe.
-            * @param {Object} responseData.data.recipe - The recipe object.
-            */
-    let { recipe } = data.data;
-    return{
-        cookingTime: recipe.cooking_time,
-        id: recipe.id,
-        imageurl: recipe.image_url,
-        publisher: recipe.publisher,
-        servings: recipe.servings,
-        sourceurl: recipe.source_url,
-        title: recipe.title,
-        ingredients: recipe.ingredients,
-        ...(recipe.key && { key: recipe.key }),
-    };
+  let { recipe } = data.data;
+  return {
+    cookingTime: recipe.cooking_time,
+    id: recipe.id,
+    imageurl: recipe.image_url,
+    publisher: recipe.publisher,
+    servings: recipe.servings,
+    sourceurl: recipe.source_url,
+    title: recipe.title,
+    ingredients: recipe.ingredients,
+    ...(recipe.key && { key: recipe.key }),
+  };
 }
 
+/**
+ * Loads a recipe by its ID and updates the state.
+ * 
+ * @param {string} id - The ID of the recipe to load.
+ * @returns {Promise<void>} A promise that resolves when the recipe is loaded.
+ * @throws Will throw an error if the recipe cannot be loaded.
+ */
 export const loadRecipe = async function (id) {
-    try {
-        //Recipe Api
-        const responseData = await AJAX(`${API_URL}${id}?key=${KEY}`);
+  try {
+    const responseData = await AJAX(`${API_URL}${id}?key=${KEY}`);
+    state.recipe = createRecipeObject(responseData);
 
-        state.recipe = createRecipeObject(responseData);
-        // console.log(state.recipe);
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else
+      state.recipe.bookmarked = false;
 
-        if (state.bookmarks.some(bookmark => bookmark.id === id))
-            state.recipe.bookmarked = true;
-        else
-            state.recipe.bookmarked = false;
-
-
-    } catch (err) {
-        // console.error(`${err} It is a error`);
-        throw err;
-    }
+  } catch (err) {
+    throw err;
+  }
 }
 
 /**
  * Loads search results based on the provided query.
- * 
- * This function fetches data from the Recipe API using the provided query,
- * processes the data to extract relevant recipe information, and updates
- * the state with the search results.
  * 
  * @param {string} query - The search query to fetch recipes for.
  * @returns {Promise<void>} A promise that resolves when the search results are loaded.
  * @throws Will log an error to the console if the fetch operation fails.
  */
 export const loadSearchResult = async function (query) {
-    try {
-        //Recipe Api
-        const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
-        // console.log(data);
+  try {
+    const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
+    state.searchs.results = data.data.recipes.map((recipe) => {
+      return {
+        id: recipe.id,
+        imageurl: recipe.image_url,
+        publisher: recipe.publisher,
+        title: recipe.title,
+        ...(recipe.key && { key: recipe.key }),
+      }
+    });
+    state.searchs.page = 1;
 
-        state.searchs.results = data.data.recipes.map((recipe) => {
-            return {
-                id: recipe.id,
-                imageurl: recipe.image_url,
-                publisher: recipe.publisher,
-                title: recipe.title,
-                ...(recipe.key && { key: recipe.key }),
-            }
-        });
-        // console.log(state.searchs.results);
-        // Here we reset the search page when we hit the api second time with new recipe
-        state.searchs.page = 1;
-
-    }
-    catch (error) {
-        console.error(error);
-    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
-     * Calculates the ending index for pagination based on the current page and results per page.
-     * 
-     * @constant {number} endWith - The ending index for the current page.
-     * @param {number} page - The current page number.
-     * @param {Object} state - The state object containing search results.
-     * @param {Object} state.searchs - The search results object.
-     * @param {number} state.searchs.resultsPerPage - The number of results per page.
-     */
-
+ * Gets search results for a specific page.
+ * 
+ * @param {number} [page=state.searchs.page] - The page number to get results for.
+ * @returns {Array<Object>} The search results for the specified page.
+ */
 export const getSearchResultsPage = function (page = state.searchs.page) {
-    state.searchs.page = page;
-    const startWith = (page - 1) * state.searchs.resultsPerPage;
-    const endWith = page * state.searchs.resultsPerPage;
-    // console.log(startWith, endWith);
-
-    return state.searchs.results.slice(startWith, endWith);
+  state.searchs.page = page;
+  const startWith = (page - 1) * state.searchs.resultsPerPage;
+  const endWith = page * state.searchs.resultsPerPage;
+  return state.searchs.results.slice(startWith, endWith);
 }
 
+/**
+ * Updates the servings for the current recipe.
+ * 
+ * @param {number} newServings - The new number of servings.
+ */
 export const updateServings = function (newServings) {
-    if (!state.recipe.ingredients || !state.recipe.servings) {
-        console.error('No ingredients or servings found in the recipe.');
-        return;
-    }
+  if (!state.recipe.ingredients || !state.recipe.servings) {
+    console.error('No ingredients or servings found in the recipe.');
+    return;
+  }
 
-    if (newServings <= 0) {
-        console.error('Servings must be greater than 0.');
-        return;
-    }
+  if (newServings <= 0) {
+    console.error('Servings must be greater than 0.');
+    return;
+  }
 
-    state.recipe.ingredients.forEach(ing => {
-        ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
-    });
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  });
 
-    state.recipe.servings = newServings;
+  state.recipe.servings = newServings;
 };
 
+/**
+ * Persists the bookmarks to local storage.
+ */
 const persistBookmarks = function () {
-    localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks))
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks))
 }
 
+/**
+ * Adds a recipe to the bookmarks.
+ * 
+ * @param {Object} recipe - The recipe to add to the bookmarks.
+ */
 export const addBookmark = function (recipe) {
+  state.bookmarks.push(recipe);
 
-    // Add recipe as a Bookmark
-    state.bookmarks.push(recipe);
-    console.log(state.bookmarks);
-
-    // Mark current recipe as bookmark
-    if (recipe.id === state.recipe.id) {
-        state.recipe.bookmarked = true;
-    }
-    persistBookmarks();
+  if (recipe.id === state.recipe.id) {
+    state.recipe.bookmarked = true;
+  }
+  persistBookmarks();
 }
 
+/**
+ * Deletes a recipe from the bookmarks.
+ * 
+ * @param {string} id - The ID of the recipe to delete from the bookmarks.
+ */
 export const deleteBookmark = function (id) {
-    const index = state.bookmarks.findIndex(bookmark => bookmark.id === id);
-    if (index !== -1) state.bookmarks.splice(index, 1);
+  const index = state.bookmarks.findIndex(bookmark => bookmark.id === id);
+  if (index !== -1) state.bookmarks.splice(index, 1);
 
-    // Mark current recipe as not bookmarked
-    if (id === state.recipe.id) {
-        state.recipe.bookmarked = false;
-    }
-    persistBookmarks();
+  if (id === state.recipe.id) {
+    state.recipe.bookmarked = false;
+  }
+  persistBookmarks();
 }
 
+/**
+ * Uploads a new recipe and updates the state.
+ * 
+ * @param {Object} newRecipe - The new recipe data.
+ * @returns {Promise<void>} A promise that resolves when the recipe is uploaded.
+ * @throws Will throw an error if the recipe cannot be uploaded.
+ */
 export const uploadRecipe = async function (newRecipe) {
-    try {
-        console.log(Object.entries(newRecipe));
-        const ingredients = Object.entries(newRecipe).filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '').map(ing => {
-            const ingArr = ing[1].split(',').map(el => el.trim());
-            // const ingArr = ing[1].replaceAll(' ', '').split(',');
-            if (ingArr.length !== 3) {
-                throw new Error('Wrong ingredient format! Please use the correct format :)');
-            }
-            const [quantity, unit, description] = ingArr;
-            return { quantity: quantity ? +quantity : null, unit, description };
-        });
-        console.log(ingredients);
-        const recipe = {
-            title: newRecipe.title,
-            source_url: newRecipe.sourceUrl,
-            image_url: newRecipe.image,
-            publisher: newRecipe.publisher,
-            cooking_time: +newRecipe.cookingTime,
-            servings: +newRecipe.servings,
-            ingredients,
-        }
-          
-       const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
+  try {
+    const ingredients = Object.entries(newRecipe).filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '').map(ing => {
+      const ingArr = ing[1].split(',').map(el => el.trim());
+      if (ingArr.length !== 3) {
+        throw new Error('Wrong ingredient format! Please use the correct format :)');
+      }
+      const [quantity, unit, description] = ingArr;
+      return { quantity: quantity ? +quantity : null, unit, description };
+    });
 
-       state.recipe = createRecipeObject(data);
-        // console.log(data);
-
-        addBookmark(state.recipe);
-    } catch (err) {
-        throw err;
+    const recipe = {
+      title: newRecipe.title,
+      source_url: newRecipe.sourceUrl,
+      image_url: newRecipe.image,
+      publisher: newRecipe.publisher,
+      cooking_time: +newRecipe.cookingTime,
+      servings: +newRecipe.servings,
+      ingredients,
     }
+
+    const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
+    state.recipe = createRecipeObject(data);
+    addBookmark(state.recipe);
+  } catch (err) {
+    throw err;
+  }
 }
 
-
-    const init = function () {
-        const storage = localStorage.getItem('bookmarks');
-        if (storage) state.bookmarks = JSON.parse(storage);
-    }
-    // console.log(init());
-    init();
+/**
+ * Initializes the application by loading bookmarks from local storage.
+ */
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+}
+init();
 
 
--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,62 +1,45 @@
-/**
- * Class representing the view for displaying search results.
- * @extends View
- */
-import View from "./View";
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
-class addRecipeView extends View {
-   _parentElement = document.querySelector(".upload");
-   _message = "Recipe was successfully uploaded";
-   _window = document.querySelector(".add-recipe-window");
-   _overlay = document.querySelector(".overlay");
-   _btnOpen = document.querySelector(".nav__btn--add-recipe");
-   _btnClose = document.querySelector(".btn--close-modal");
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _message = 'Recipe was successfully uploaded :)';
 
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
 
-   constructor() {
-      super();
-      this._addHandlerShowWindow;
-      this._addHandlerHideWindow;
-   }
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
 
-    toggleWindow() {
-      this._window.classList.toggle("hidden");
-      this._overlay.classList.toggle("hidden");
-      if (this._window.classList.contains("hidden")) {
-         this._parentElement.reset(); // Reset the form fields
-      }
-   }
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
 
-   addHandlerShowWindow = function () {
-      this._btnOpen.addEventListener("click", this.toggleWindow.bind(this));
-   }
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
 
-   addHandlerHideWindow = function () {
-      this._btnClose.addEventListener("click", this.toggleWindow.bind(this));
-      this._overlay.addEventListener("click", this.toggleWindow.bind(this));
-   }
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
 
-   addHandlerUpload(handler){
-      this._parentElement.addEventListener("submit",function(e){
-         e.preventDefault();
-         const dataArr = [...new FormData(this)];
-         const data = Object.fromEntries(dataArr);
-         handler(data);
-      })
-   }
-   /**
-    * Generates the markup for the results view.
-    * 
-    * This method maps over the data and generates a markup preview for each item,
-    * then joins them into a single string.
-    * 
-    * @retxurns {string} The generated markup as a single string.
-    */
-   _genrateMarkup() {
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr);
+      handler(data);
+    });
+  }
 
-   }
-
-
+  _generateMarkup() {}
 }
 
-export default new addRecipeView();
+export default new AddRecipeView();
